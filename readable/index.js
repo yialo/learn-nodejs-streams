2b60c05287ef14a6fde9e99445f95104107b56a0
Aleksei Arro <polsck@gmail.com>
2021-02-26 13:30:36 +0300

    Add error handler

--- readable/index.js
+++ readable/index.js
@@ -1,10 +1,13 @@
 import fs from 'fs';
 
-const stream = fs.createReadStream('./readable/video.mp4');
+const stream = fs.createReadStream('./readable/video1.mp4');
 
 stream.on('data', (chunk) => {
   console.log(chunk);
 });
 stream.on('end', () => {
   console.log('End');
+});
+stream.on('error', (error) => {
+  console.log(error.message);
 });
