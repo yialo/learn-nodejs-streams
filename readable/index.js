4d2db6a2a4398eb584e9ac0bc03ef602c4c68b09
Aleksei Arro <polsck@gmail.com>
2021-02-26 13:41:36 +0300

    Add new event handlers to readable stream

--- readable/index.js
+++ readable/index.js
@@ -1,17 +1,25 @@
 import fs from 'fs';
 
-const stream = fs.createReadStream('./readable/text.txt', { encoding: 'utf8' });
+const initEventDrivenApproach = () => {
+  const stream = fs.createReadStream('./readable/text.txt', { encoding: 'utf8' });
 
-// NOTE: alternative variant
-// const stream = fs.createReadStream('./readable/text.txt');
-// stream.setEncoding('utf8');
+  // NOTE: alternative variant
+  // const stream = fs.createReadStream('./readable/text.txt');
+  // stream.setEncoding('utf8');
 
-stream.on('data', (chunk) => {
-  console.log(chunk);
-});
-stream.on('end', () => {
-  console.log('End');
-});
-stream.on('error', (error) => {
-  console.log(error.message);
-});
+  stream.on('data', (chunk) => {
+    console.log(chunk);
+  });
+  stream.on('end', () => {
+    console.log('[End of file]');
+  });
+  stream.on('error', (error) => {
+    console.log(error.message);
+  });
+  stream.on('close', () => {
+    console.log('[Close file]');
+  });
+  stream.on('open', () => {
+    console.log('[Open file]');
+  });
+};
