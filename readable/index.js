f9cc2e09701b31c3f3a09c69949d9c70b1ccfa44
Aleksei Arro <polsck@gmail.com>
2021-02-26 13:34:09 +0300

    Add reading of text file

--- readable/index.js
+++ readable/index.js
@@ -1,6 +1,10 @@
 import fs from 'fs';
 
-const stream = fs.createReadStream('./readable/video1.mp4');
+const stream = fs.createReadStream('./readable/text.txt', { encoding: 'utf8' });
+
+// NOTE: alternative variant
+// const stream = fs.createReadStream('./readable/text.txt');
+// stream.setEncoding('utf8');
 
 stream.on('data', (chunk) => {
   console.log(chunk);
